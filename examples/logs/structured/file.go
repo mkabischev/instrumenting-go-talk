d5a93b632dafe22cedc4ef40fe871262bd202017
Mike Kabischev <kabischev@gmail.com>
2017-05-24 14:27:00 +0300

    update

---
+++ examples/logs/structured/file.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+
+	"junolab.net/ms/core/logger"
+)
+
+type Logger interface{}
+
+// START OMIT
+const key int = 0
+
+func ToContext(ctx context.Context, l Logger) context.Context {
+	return context.WithValue(ctx, key, l)
+}
+
+func FromContext(ctx context.Context) Logger {
+	return ctx.Value(key).(Logger)
+}
+
+// END OMIT
+
+type Request struct{}
+type Response struct{}
+
+// HANDLER_START OMIT
+
+func SomeHandler(ctx context.Context, r Request) (*Response, error) {
+	l := logger.FromContext(ctx)  // HLx
+	l.Debug("something happened") // HLx
+
+	// ...
+
+}
+
+// HANDLER_END OMIT
+
+// PREPARE_START OMIT
+l = l.WithField("request_id", NewRequestID()) // HLx
+ctx := logger.WithContext(ctx, l) // HLx
+
+// ...
+
+res, err := SomeHandler(ctx, req)
+
+// PREPARE_END OMIT
